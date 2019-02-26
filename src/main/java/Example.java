5d5e3f22e15bf7bbcb279c38349b4da4a36ab594
Benjamin Henrion <zoobab@gmail.com>
2019-02-26 15:10:10 +0100

    15H10

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -8,7 +8,7 @@ public class Example {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello World 15H10!";
 	}
 
 	public static void main(String[] args) throws Exception {
